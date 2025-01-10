9b80931a3efacedeedf41516a0d34eab91af6e89
Marco Polo Jácome Toss <jacometoss@gmail.com>
2025-01-10 17:54:45 -0600

    10.01.2025_1
    Actualización del theme: sphinxdoc

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ html_show_sourcelink = True
 # a list of builtin themes.
 #
 #html_theme = 'bizstyle'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinxdoc'
 html_search_language = 'es'
 #html_theme = 'bizstyle'
 
